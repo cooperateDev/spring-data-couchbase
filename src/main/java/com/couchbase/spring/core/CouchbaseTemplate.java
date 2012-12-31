2b97388f1be80a603f1b8e926ea93c45b0ad71a9
cooperateDev <pavelasanov1206@gmail.com>
2012-12-31 09:21:51 +0100

    Extending basic support for mapping and adding an abstract configuration.

---
+++ src/main/java/com/couchbase/spring/core/CouchbaseTemplate.java
@@ -0,0 +1,51 @@
+/**
+ * Copyright (C) 2009-2012 Couchbase, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALING
+ * IN THE SOFTWARE.
+ */
+
+package com.couchbase.spring.core;
+
+import com.couchbase.client.CouchbaseClient;
+import com.couchbase.spring.core.convert.CouchbaseConverter;
+import com.couchbase.spring.core.convert.MappingCouchbaseConverter;
+
+public class CouchbaseTemplate implements CouchbaseOperations {
+
+  private CouchbaseClient client;
+  private CouchbaseConverter couchbaseConverter;
+
+  public CouchbaseTemplate(CouchbaseClient client) {
+    this(client, null);
+  }
+
+  public CouchbaseTemplate(CouchbaseClient client, CouchbaseConverter converter) {
+    this.client = client;
+    this.couchbaseConverter = converter == null ? getDefaultConverter(client) : converter;
+  }
+
+  private CouchbaseConverter getDefaultConverter(CouchbaseClient client) {
+    MappingCouchbaseConverter converter = new MappingCouchbaseConverter(
+      new CouchbaseMappingContext());
+    converter.afterPropertiesSet();
+    return converter;
+  }
+
+
+}
